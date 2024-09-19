ffdde9d79b966a365e31e79f04e8ba85871018a4
ivan <tecno85@gmail.com>
2024-09-18 19:16:04 -0500

    Iniciando Listas

---
+++ _06_lists.py
@@ -0,0 +1,15 @@
+""" List """
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [1, 2, 3, "cuatro", 5.0]
+my_list[3] = "Cuatro Modificado"
+print(my_list)
+
+my_list.append(6)
+print(my_list)
+my_list.append("Ivan")
+print(my_list)
