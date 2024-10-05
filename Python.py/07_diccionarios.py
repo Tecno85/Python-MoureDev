fc6f6e4475f4347978e46138aae959e6ee263d5c
ivan <tecno85@gmail.com>
2024-10-05 10:55:04 -0500

    Tratando de dar orden al repo

---
+++ Python.py/07_diccionarios.py
@@ -0,0 +1,13 @@
+my_dict = {
+    "nombre": "Ivan",
+    "Apellido": "Madrid",
+    "Edad": 39,
+    2: 850904
+}
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.fromkeys(("Ciudad", "Cantidad")))
+
+# Tiempo: 5:40
